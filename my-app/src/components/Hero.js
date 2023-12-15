8539b52f66e2bc7b5ea04439326fb8150d255f8c
seigfred0 <saysonseigfred@gmail.com>
2023-12-15 21:24:34 +0800

    update

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -8,8 +8,11 @@ const Hero = () => {
                 <img src={headerImage} alt="header"/>
             </div>
             <div className="hero-section_text">
-                <h1>  Bring everyone together to build better products.</h1>
-                <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
+                <h1 className="main-heading heading">Bring everyone together to build better products.</h1>
+                <p className="text-main text">Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
+                <div className="btn-container">
+                    <button className="btn">Get Started</button>
+                </div>
             </div>
         </section>
     );
