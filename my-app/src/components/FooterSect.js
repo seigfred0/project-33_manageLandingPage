5884c753c1cc8fddab029d8a60c3b9e6d705dd2d
seigfred0 <saysonseigfred@gmail.com>
2023-12-19 09:20:04 +0800

    footer in progress

--- my-app/src/components/FooterSect.js
+++ my-app/src/components/FooterSect.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import facebook from '../images/icon-facebook.svg';
+import instagram from '../images/icon-instagram.svg';
+import pinterest from '../images/icon-pinterest.svg';
+import twitter from '../images/icon-twitter.svg';
+import youtube from '../images/icon-youtube.svg';
+
 
 const FooterSect = () => {
 
     return (
         <section>
             <CallToAction></CallToAction>
+            <Footer></Footer>
         </section>
     )
 }
@@ -22,6 +29,40 @@ const CallToAction = () => {
 
 const Footer = () => {
     
+    return (
+        <div className='footer-section'>
+            <div className='footer-email'>
+                <form>
+                    <input type='email' placeholder='Update in your inbox...' />
+                    <button>GO</button>
+                </form>
+            </div>
+            <div className='footer-links'>
+                <ul>
+                    <li>Home</li>
+                    <li>Pricing</li>
+                    <li>Products</li>
+                    <li>About Us</li>
+                </ul>
+                <ul>
+                    <li>Careers</li>
+                    <li>Community</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className='footer-social'>
+                <div className='footer-social_icons'>
+                    <img src={facebook} alt='facebook' />
+                    <img src={youtube} alt='youtube' />
+                    <img src={twitter} alt='twitter' />
+                    <img src={pinterest} alt='pinterest' />
+                    <img src={instagram} alt='instagram' />
+                </div>
+                <div className='footer-social_logo'></div>
+            </div>
+
+        </div>
+    )
 }
 
 export default FooterSect;
